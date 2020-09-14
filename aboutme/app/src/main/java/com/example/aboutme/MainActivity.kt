2142f73cc6b972eccecc5146cedd938a86b7322f
Daniel Prado <dfe.prado@gmail.com>
2020-09-14 06:51:45 -0300

    AboutMe: Finalizado

--- aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.example.aboutme
-
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View;
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Button;
 import androidx.databinding.DataBindingUtil
 import com.example.aboutme.databinding.ActivityMainBinding
-import com.example.aboutme.MyName;
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,23 +15,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.myName = myName;
         binding.doneButton.setOnClickListener{
             setNickname(it);
         }
-
-        binding.myName = myName;
-        //setContentView(R.layout.activity_main);
-        //findViewById<Button>(R.id.done_button).setOnClickListener {
-        //    setNickname(it);
-        //};
     }
 
     fun setNickname(view: View) {
-        //val edit: EditText = findViewById(R.id.nickname_edit);
-        //val nickname: TextView = findViewById(R.id.nickname_text);
-
         binding.apply {
-            myName?.nickname = nicknameEdit.text.toString();
             nicknameText.visibility = View.VISIBLE;
             nicknameEdit.visibility = View.GONE;
             doneButton.visibility = View.GONE;
