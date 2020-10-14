9b9b45122d86ddd22fec40c0afa9d050e2daad60
Daniel Prado <dfe.prado@gmail.com>
2020-10-14 07:03:04 -0300

    +SleepTrackerViewModel and Factory

--- sleeptracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ sleeptracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -16,13 +16,17 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.app.Application
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepDatabase
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
 
 /**
@@ -43,6 +47,14 @@ class SleepTrackerFragment : Fragment() {
         // Get a reference to the binding object and inflate the fragment views.
         val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_sleep_tracker, container, false)
+
+        val application: Application = requireNotNull(this.activity).application;
+        val database: SleepDatabaseDao = SleepDatabase.getInstance(application).sleepDatabaseDao;
+        val viewModelFactory: SleepTrackerViewModelFactory = SleepTrackerViewModelFactory(database, application);
+        val viewModel: SleepTrackerViewModel = ViewModelProviders.of(this, viewModelFactory).get(SleepTrackerViewModel::class.java);
+
+        binding.setLifecycleOwner(this);
+        binding.sleepTrackerViewModel = viewModel;
 
         return binding.root
     }
