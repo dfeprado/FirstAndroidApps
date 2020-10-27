45499951f2edf13e6bf285e52a00cd2d9038312f
Daniel Prado <dfe.prado@gmail.com>
2020-10-27 06:55:07 -0300

    Refatoração do RecyclerViewAdapter

--- sleeptracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ sleeptracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -24,31 +24,41 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int): Unit {
         val item: SleepNight = data[position];
-        val res = holder.itemView.resources;
-        holder.sleepNightDuration.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res);
-        holder.sleepQualityText.text = convertNumericQualityToString(item.sleepQuality, res);
-        holder.sleepQualityIcon.setImageResource(
-                when(item.sleepQuality) {
-                    0 -> R.drawable.ic_sleep_0;
-                    1 -> R.drawable.ic_sleep_1;
-                    2 -> R.drawable.ic_sleep_2;
-                    3 -> R.drawable.ic_sleep_3;
-                    4 -> R.drawable.ic_sleep_4;
-                    5 -> R.drawable.ic_sleep_5;
-                    else -> R.drawable.sleep_active;
-                }
-        );
+        holder.bind(item);
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context);
-        val view = layoutInflater.inflate(R.layout.sleep_night_list_item, parent, false);
-        return ViewHolder(view);
+        return ViewHolder.from(parent)
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    class ViewHolder private constructor (itemView: View): RecyclerView.ViewHolder(itemView) {
         val sleepQualityIcon: ImageView = itemView.findViewById(R.id.sleep_night_icon);
         val sleepQualityText: TextView = itemView.findViewById(R.id.sleep_night_quality);
         val sleepNightDuration: TextView = itemView.findViewById(R.id.sleep_night_duration);
+
+        fun bind(item: SleepNight): Unit {
+            val res = itemView.resources;
+            sleepNightDuration.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res);
+            sleepQualityText.text = convertNumericQualityToString(item.sleepQuality, res);
+            sleepQualityIcon.setImageResource(
+                    when (item.sleepQuality) {
+                        0 -> R.drawable.ic_sleep_0;
+                        1 -> R.drawable.ic_sleep_1;
+                        2 -> R.drawable.ic_sleep_2;
+                        3 -> R.drawable.ic_sleep_3;
+                        4 -> R.drawable.ic_sleep_4;
+                        5 -> R.drawable.ic_sleep_5;
+                        else -> R.drawable.sleep_active;
+                    }
+            );
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context);
+                val view = layoutInflater.inflate(R.layout.sleep_night_list_item, parent, false);
+                return ViewHolder(view);
+            }
+        }
     }
 }
